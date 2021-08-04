43507f19823685a2e00f9166164f4fe570835619
isaachintosh <isaac.tuning@gmail.com>
2021-08-04 01:46:13 -0300

    created classes and crud

---
+++ repositorio/estoque.py
@@ -0,0 +1,32 @@
+
+class Estoque:
+
+    self.Produtos = {}
+
+    def __init__(self):
+        pass
+
+    def create(data_json):
+        
+        if(data_json.nome == "" and data_json.nome == null):
+            return "Preencha o nome"
+        
+        if({nome, fabricante, descricao, valor, quantidade, data_validade, categoria } == ""):
+            return "Preencha todas as informações"
+
+
+    def delete(_id):
+    
+    def findAll():
+        return [
+            {
+                _id: 0,
+            },
+            {
+                _id: 1,
+            }
+        ]
+    
+    def findOne(_id)
+
+    def update(_id):
